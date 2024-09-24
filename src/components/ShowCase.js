24ef071f0f3be9e98c95c99800a1bfe944c31596
Kim Seok Jin <kimseokjin6519@gmail.com>
2024-09-24 10:14:52 +0000

    showcase - layout

--- src/components/ShowCase.js
+++ src/components/ShowCase.js
@@ -1,40 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const ShowCase = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await fetch('http://192.168.52.128:5000/api/products');
+                const data = await response.json();
+                const shuffledProducts = shuffleArray(data);
+                setProducts(shuffledProducts);
+            } catch (error) {
+                console.error('Error fetching product data:', error);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+
+    const shuffleArray = (array) => {
+        return array.sort(() => Math.random() - 0.5);
+    };
+
     return (
         <div className="flex space-x-5 w-full">
             <div className="">
-                <img src="https://via.placeholder.com/300x400" alt="Large Card 1" className="w-full h-full object-cover bg-blue-300" />
+                <img src = {products[0]?.url} alt = {products[0]?.title} className = "w-full h-full object-cover bg-blue-300" />
             </div>
             <div className="">
-                <img src="https://via.placeholder.com/300x400" alt="Large Card 2" className="w-full h-full object-cover bg-blue-300" />
+                <img src = {products[1]?.url} alt = {products[1]?.title} className = "w-full h-full object-cover bg-blue-300" />
             </div>
-            
+
             <div className="grid grid-cols-4 grid-rows-2 gap-2 ml-5">
-                <div className="">
-                    <img src="https://via.placeholder.com/200x200" alt="Small Card 1" className="w-full h-full object-cover bg-red-300" />
-                </div>
-                <div className="">
-                    <img src="https://via.placeholder.com/200x200" alt="Small Card 2" className="w-full h-full object-cover bg-red-300" />
-                </div>
-                <div className="">
-                    <img src="https://via.placeholder.com/200x200" alt="Small Card 3" className="w-full h-full object-cover bg-red-300" />
-                </div>
-                <div className="">
-                    <img src="https://via.placeholder.com/200x200" alt="Small Card 4" className="w-full h-full object-cover bg-red-300" />
-                </div>
-                <div className="">
-                    <img src="https://via.placeholder.com/200x200" alt="Small Card 5" className="w-full h-full object-cover bg-red-300" />
-                </div>
-                <div className="">
-                    <img src="https://via.placeholder.com/200x200" alt="Small Card 6" className="w-full h-full object-cover bg-red-300" />
-                </div>
-                <div className="">
-                    <img src="https://via.placeholder.com/200x200" alt="Small Card 5" className="w-full h-full object-cover bg-red-300" />
-                </div>
-                <div className="">
-                    <img src="https://via.placeholder.com/200x200" alt="Small Card 6" className="w-full h-full object-cover bg-red-300" />
-                </div>
+                {products.slice(2, 10).map((product, index) => (
+                    <div key = {index} className = "">
+                        <img src = {product.url} alt = {product.title} className = "w-full h-full object-cover bg-red-300" />
+                    </div>
+                ))}
             </div>
         </div>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,11 +17,11 @@ function Home() {
                   <div>New</div>
                </span>
             </div>
-            <div style = {{ position: 'absolute', right: '65px', marginTop: '10px' }}>
+            <div style = {{ position: 'absolute', right: '70px', marginTop: '10px' }}>
                <input type="text" placeholder="Search" className="box-border rounded-xl w-3/4 focus:outline-none text-sm bg-gray-200 border-gray-500 border pl-2" />
             </div>
-            <svg style={{ position: 'absolute', right: '75px', marginTop: '10px' }} aria-hidden="true" focusable="false" viewBox="0 0 24 24" role="img" width="21px" height="21px" fill="none"><path stroke="currentColor" stroke-width="1.5" d="M16.794 3.75c1.324 0 2.568.516 3.504 1.451a4.96 4.96 0 010 7.008L12 20.508l-8.299-8.299a4.96 4.96 0 010-7.007A4.923 4.923 0 017.205 3.75c1.324 0 2.568.516 3.504 1.451l.76.76.531.531.53-.531.76-.76a4.926 4.926 0 013.504-1.451"></path></svg>
-            <svg style={{ position: 'absolute', right: '45px', marginTop: '10px'}} aria-hidden="true" focusable="false" viewBox="0 0 24 24" role="img" width="21px" height="21px" fill="none"><path stroke="currentColor" stroke-width="1.5" d="M8.25 8.25V6a2.25 2.25 0 012.25-2.25h3a2.25 2.25 0 110 4.5H3.75v8.25a3.75 3.75 0 003.75 3.75h9a3.75 3.75 0 003.75-3.75V8.25H17.5"></path></svg>
+            <svg style={{ position: 'absolute', right: '75px', marginTop: '10px' }} aria-hidden="true" focusable="false" viewBox="0 0 24 24" role="img" width="21px" height="21px" fill="none"><path stroke="currentColor" strokeWidth="1.5" d="M16.794 3.75c1.324 0 2.568.516 3.504 1.451a4.96 4.96 0 010 7.008L12 20.508l-8.299-8.299a4.96 4.96 0 010-7.007A4.923 4.923 0 017.205 3.75c1.324 0 2.568.516 3.504 1.451l.76.76.531.531.53-.531.76-.76a4.926 4.926 0 013.504-1.451"></path></svg>
+            <svg style={{ position: 'absolute', right: '45px', marginTop: '10px'}} aria-hidden="true" focusable="false" viewBox="0 0 24 24" role="img" width="21px" height="21px" fill="none"><path stroke="currentColor" strokeWidth="1.5" d="M8.25 8.25V6a2.25 2.25 0 012.25-2.25h3a2.25 2.25 0 110 4.5H3.75v8.25a3.75 3.75 0 003.75 3.75h9a3.75 3.75 0 003.75-3.75V8.25H17.5"></path></svg>
          </div>
          <div className="w-full bg-white flex-col items-center justify-center" style = {{paddingTop: '55px'}}>    
             <div className="h-full bg-gray-400">
